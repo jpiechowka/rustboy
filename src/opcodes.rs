fc4f71292b38a6a1996027591407f995aeea20bb
Janusz Piechówka <januszpiechowka@gmail.com>
2023-08-21 17:59:54 +0200

    Add initial from_byte, from_byte_prefixed and from_byte_not_prefixed functions for Opcodes

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,3 +1,6 @@
+// https://gbdev.io/gb-opcodes/optables
+// https://gbdev.io/gb-opcodes/Opcodes.json
+
 pub enum Opcode {
     NOP,
     ADD(ArithmeticTarget),
@@ -11,4 +14,26 @@ pub enum ArithmeticTarget {
     E,
     H,
     L,
+}
+
+impl Opcode {
+    pub fn from_byte(byte: u8, is_prefixed: bool) -> Option<Opcode> {
+        if is_prefixed {
+            return Opcode::from_byte_prefixed(byte);
+        }
+        Opcode::from_byte_not_prefixed(byte)
+    }
+
+    fn from_byte_prefixed(byte: u8) -> Option<Opcode> {
+        match byte {
+            _ => None,
+        }
+    }
+
+    fn from_byte_not_prefixed(byte: u8) -> Option<Opcode> {
+        match byte {
+            0x00 => Some(Opcode::NOP),
+            _ => None,
+        }
+    }
 }
