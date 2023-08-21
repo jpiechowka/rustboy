c11bf239c1e8ebfa9881c0bd8b820a03426629a6
Janusz Piechówka <januszpiechowka@gmail.com>
2023-08-21 17:58:42 +0200

    Make read_byte public

--- src/memory_bus.rs
+++ src/memory_bus.rs
@@ -3,7 +3,7 @@ pub struct MemoryBus {
 }
 
 impl MemoryBus {
-    fn read_byte(&self, address: u16) -> u8 {
+    pub fn read_byte(&self, address: u16) -> u8 {
         self.memory[address as usize]
     }
 }
