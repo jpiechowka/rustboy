068cead594a187c111be80abaec2d705ced98850
Janusz Piechówka <januszpiechowka@gmail.com>
2023-08-23 00:02:58 +0200

    Fully implement loading opcodes from JSON. Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod rom;
 use clap::Parser;
 use env_logger::Builder;
 use log::{debug, error, info, LevelFilter};
+use opcodes_json::Opcodes;
 use rom::Rom;
 
 const ROM_FILEPATH: &str = "roms/cpu_instrs.gb";
@@ -21,10 +22,19 @@ struct Cli {
 }
 
 fn main() {
-    let cli = Cli::parse();
+    // let cli = Cli::parse(); // TODO: implement CLI
 
     let mut log_builder = Builder::from_default_env();
     log_builder.filter_level(LevelFilter::Trace).init(); // TODO: provide way to set this using env var
+
+    // TODO: move this part of code
+    let opcodes = Opcodes::new();
+    match opcodes.unprefixed.get("0x00") {
+        Some(opcode_details) => {
+            info!("{:#?}", opcode_details);
+        }
+        None => panic!("Unable to get opcode details from HashMap"),
+    }
 
     debug!("Loading ROM from file: {}", ROM_FILEPATH);
     match Rom::load_from_file(ROM_FILEPATH) {
--- src/opcodes_json.rs
+++ src/opcodes_json.rs
@@ -1,23 +1,69 @@
 // https://gbdev.io/gb-opcodes/optables
 // https://gbdev.io/gb-opcodes/Opcodes.json
 
+use std::collections::HashMap;
+
+use log::info;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Opcode {
-    flags: Flags,
+// TODO: check visibility on structs and functions
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Opcodes {
+    pub unprefixed: HashMap<String, OpcodeDetails>,
+    pub cbprefixed: HashMap<String, OpcodeDetails>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Flags {
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct OpcodeDetails {
+    pub mnemonic: String, // Illegal opcodes are prefixed with ILLEGAL, (for example: ILLEGAL_D3 from unprefixed)
+    pub bytes: u8,
+    pub cycles: Vec<u8>,
+    pub operands: Vec<Operand>,
+    pub immediate: bool,
+    pub flags: Flags,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Operand {
+    pub name: String,
+    #[serde(default)]
+    pub bytes: u8,
+    pub immediate: bool,
+}
+
+// Z - Zero Flag
+// N - Subtract Flag
+// H - Half Carry Flag
+// C - Carry Flag
+// 0 - The flag is reset
+// 1 - The flag is set
+// - - The flag is left untouched
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Flags {
     #[serde(rename = "Z")]
-    zero: String,
+    pub zero: String,
     #[serde(rename = "N")]
-    subtract: String,
+    pub subtract: String,
     #[serde(rename = "H")]
-    half_carry: String,
+    pub half_carry: String,
     #[serde(rename = "C")]
-    carry: String,
+    pub carry: String,
+}
+
+impl Opcodes {
+    pub fn new() -> Opcodes {
+        info!("Loading opcodes from JSON");
+        let json_opcodes = include_str!("opcodes/opcodes.json");
+        let opcodes: Opcodes =
+            serde_json::from_str(json_opcodes).expect("should load opcodes form JSON");
+
+        info!("Opcodes loaded successfully and ready to use");
+        Opcodes {
+            unprefixed: opcodes.unprefixed,
+            cbprefixed: opcodes.cbprefixed,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -25,29 +71,154 @@ mod tests {
     use super::*;
 
     #[test]
-    fn deserialize_opcode_from_json() {
+    fn can_init_opcodes() {
+        let opcodes = Opcodes::new();
+        assert_eq!(opcodes.unprefixed.len(), 256);
+        assert_eq!(opcodes.cbprefixed.len(), 256);
+
+        let unprefixed_0xCE_opcode = opcodes.unprefixed.get("0xCE").unwrap();
+        assert_eq!(unprefixed_0xCE_opcode.mnemonic, "ADC");
+        assert_eq!(unprefixed_0xCE_opcode.bytes, 2);
+        assert_eq!(unprefixed_0xCE_opcode.cycles, vec![8]);
+        assert_eq!(
+            unprefixed_0xCE_opcode.operands,
+            vec![
+                Operand {
+                    name: "A".to_string(),
+                    bytes: 0,
+                    immediate: true,
+                },
+                Operand {
+                    name: "n8".to_string(),
+                    bytes: 1,
+                    immediate: true,
+                }
+            ]
+        );
+        assert!(unprefixed_0xCE_opcode.immediate);
+        assert_eq!(unprefixed_0xCE_opcode.flags.zero, "Z");
+        assert_eq!(unprefixed_0xCE_opcode.flags.subtract, "0");
+        assert_eq!(unprefixed_0xCE_opcode.flags.half_carry, "H");
+        assert_eq!(unprefixed_0xCE_opcode.flags.carry, "C");
+
+        let cbprefixed_0x00_opcode = opcodes.cbprefixed.get("0x00").unwrap();
+        assert_eq!(cbprefixed_0x00_opcode.mnemonic, "RLC");
+        assert_eq!(cbprefixed_0x00_opcode.bytes, 2);
+        assert_eq!(cbprefixed_0x00_opcode.cycles, vec![8]);
+        assert_eq!(
+            cbprefixed_0x00_opcode.operands,
+            vec![Operand {
+                name: "B".to_string(),
+                bytes: 0,
+                immediate: true,
+            },]
+        );
+        assert!(cbprefixed_0x00_opcode.immediate);
+        assert_eq!(cbprefixed_0x00_opcode.flags.zero, "Z");
+        assert_eq!(cbprefixed_0x00_opcode.flags.subtract, "0");
+        assert_eq!(cbprefixed_0x00_opcode.flags.half_carry, "0");
+        assert_eq!(cbprefixed_0x00_opcode.flags.carry, "C");
+    }
+
+    #[test]
+    fn can_deserialize_unprefixed_opcode_details_from_json() {
         let json = r#"
         {
-            "mnemonic": "DEC",
-            "bytes": 1,
-            "cycles": [4],
-            "operands": [
-                {
-                    "name": "B",
-                    "immediate": true
-                }
-            ],
-            "immediate": true,
-            "flags": {
-                "Z": "Z",
-                "N": "1",
-                "H": "H",
-                "C": "-"
-            }
-        }
+			"mnemonic": "ADC",
+			"bytes": 2,
+			"cycles": [
+				8
+			],
+			"operands": [
+				{
+					"name": "A",
+					"immediate": true
+				},
+				{
+					"name": "n8",
+					"bytes": 1,
+					"immediate": true
+				}
+			],
+			"immediate": true,
+			"flags": {
+				"Z": "Z",
+				"N": "0",
+				"H": "H",
+				"C": "C"
+			}
+		}
     "#;
 
-        let opcode: Opcode = serde_json::from_str(json).unwrap();
-        println!("{:?}", opcode)
+        let opcode: OpcodeDetails = serde_json::from_str(json).unwrap();
+
+        assert_eq!(opcode.mnemonic, "ADC");
+        assert_eq!(opcode.bytes, 2);
+        assert_eq!(opcode.cycles, vec![8]);
+        assert_eq!(
+            opcode.operands,
+            vec![
+                Operand {
+                    name: "A".to_string(),
+                    bytes: 0,
+                    immediate: true,
+                },
+                Operand {
+                    name: "n8".to_string(),
+                    bytes: 1,
+                    immediate: true,
+                }
+            ]
+        );
+        assert!(opcode.immediate);
+        assert_eq!(opcode.flags.zero, "Z");
+        assert_eq!(opcode.flags.subtract, "0");
+        assert_eq!(opcode.flags.half_carry, "H");
+        assert_eq!(opcode.flags.carry, "C");
+    }
+
+    #[test]
+    fn can_deserialize_prefixed_opcode_details_from_json() {
+        let json = r#"
+        {
+			"mnemonic": "RLC",
+			"bytes": 2,
+			"cycles": [
+				8
+			],
+			"operands": [
+				{
+					"name": "B",
+					"immediate": true
+				}
+			],
+			"immediate": true,
+			"flags": {
+				"Z": "Z",
+				"N": "0",
+				"H": "0",
+				"C": "C"
+			}
+		}
+    "#;
+
+        let opcode: OpcodeDetails = serde_json::from_str(json).unwrap();
+
+        assert_eq!(opcode.mnemonic, "RLC");
+        assert_eq!(opcode.bytes, 2);
+        assert_eq!(opcode.cycles, vec![8]);
+        assert_eq!(
+            opcode.operands,
+            vec![Operand {
+                name: "B".to_string(),
+                bytes: 0,
+                immediate: true,
+            },]
+        );
+        assert!(opcode.immediate);
+        assert_eq!(opcode.flags.zero, "Z");
+        assert_eq!(opcode.flags.subtract, "0");
+        assert_eq!(opcode.flags.half_carry, "0");
+        assert_eq!(opcode.flags.carry, "C");
     }
 }
